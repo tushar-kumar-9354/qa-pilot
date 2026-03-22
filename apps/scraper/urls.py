fa828d340508091f1ce58231f0eba28f877597e1
kpregtech <kpregtech@gmail.com>
2026-03-22 20:25:52 +0530

    fix: add Pillow to integration and e2e CI jobs

--- apps/scraper/urls.py
+++ apps/scraper/urls.py
@@ -1,2 +1,7 @@
-from django.urls import path 
-urlpatterns = [] 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('trigger', views.trigger_scraper, name='trigger_scraper'),
+    path('data', views.list_scraped_data, name='list_scraped_data'),
+]
